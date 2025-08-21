954cc90b3b83d5028c19f25e5f6645a2a6be8a84
Jay-Allemand Maxime <maxime.jay.allemand@hydris-hydrologie.fr>
2025-08-21 15:12:47 +0200

    remove conflit item

--- smash/factory/mesh/mesh.py
+++ smash/factory/mesh/mesh.py
@@ -682,8 +682,4 @@ def _generate_mesh(
     if bbox is not None and x is None:
         return _generate_mesh_from_bbox(flwdir_dataset, bbox, epsg)
     else:
-<<<<<<< HEAD
-        return _generate_mesh_from_xy(flwdir_dataset, bbox, x, y, area, code, shp_dataset, max_depth, epsg)
-=======
         return _generate_mesh_from_xy(flwdir_dataset, x, y, area, code, shp_dataset, max_depth, epsg, area_error_th)
->>>>>>> exclude_mesh_outlets_based_on_surface_error
